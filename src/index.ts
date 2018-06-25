26a746250533e480df31ffebb5420ae105cc8368
Adam Mahood <mahood.adam@gmail.com>
2018-06-25 00:11:14 -0400

    src: export types in index file

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,14 @@ const { token }: { token: string } = require('../settings.json');
 const client: Client = new Client();
 
 client.login(token);
+
+// Export Types
+import { ModLog } from '../lib/moderation/ModLog';
+
+export {
+  // Client
+  Client as NectarClient,
+
+  // Classes
+  ModLog
+};
